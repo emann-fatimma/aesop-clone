47d12a99167911ef1cd8e03197063e4bd9d2ae4e
emann-fatimma <emannadeem8998@gmail.com>
2025-05-27 01:56:17 +0500

    COMMITING CHANGES

--- aesop-backend/config/middlewares.ts
+++ aesop-backend/config/middlewares.ts
@@ -1,3 +1,32 @@
+// export default [
+//   'strapi::logger',
+//   'strapi::errors',
+//   'strapi::security',
+//   {
+//     name: 'strapi::cors',
+//     config: {
+//       enabled: true,
+//       headers: '*',
+//       origin: [
+//         'http://localhost:3000',
+//         'http://127.0.0.1:3000',
+//         'http://localhost:3001',
+//         'http://127.0.0.1:3001',
+//         'https://aesop-webapp-3uh9jtdl3-eman-fatimas-projects-07a3e151.vercel.app/', // Replace with your actual Vercel URL
+//         'https://*.vercel.app', // This allows all Vercel subdomains
+//         // Add your custom domain if you have one
+//         // 'https://yourdomain.com'
+//       ]
+//     }
+//   },
+//   'strapi::poweredBy',
+//   'strapi::query',
+//   'strapi::body',
+//   'strapi::session',
+//   'strapi::favicon',
+//   'strapi::public',
+// ];
+
 export default [
   'strapi::logger',
   'strapi::errors',
@@ -12,9 +41,9 @@ export default [
         'http://127.0.0.1:3000',
         'http://localhost:3001',
         'http://127.0.0.1:3001',
-        'https://aesop-webapp-3uh9jtdl3-eman-fatimas-projects-07a3e151.vercel.app/', // Replace with your actual Vercel URL
+        'https://aesop-clone-p86t-git-main-eman-fatimas-projects-07a3e151.vercel.app',
         'https://*.vercel.app', // This allows all Vercel subdomains
-        // Add your custom domain if you have one
+        // Add custom domain if you have one
         // 'https://yourdomain.com'
       ]
     }
